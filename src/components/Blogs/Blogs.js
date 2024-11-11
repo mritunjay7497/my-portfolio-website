c6f76bf4bd86f2c3e3046856b8a78f141837a91e
= <=>
2024-11-11 18:06:01 +0530

    Stylings for blog card

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,18 +4,13 @@ import Particle from "../Particle";
 import BlogParser from "./BlogParser";
 
 function Blogs({blogUrl}) {
-    
+
     return (
         <Container fluid className="about-section">
             <Particle />
             <Container>
                 <Row style={{ justifyContent: "center", padding: "10px" }}>
-                    <Col
-                        md={12}
-                    >
-                        <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-                            <strong className="purple">Blogs</strong>
-                        </h1>
+                    <Col md={12}>
                         <div className="blogs-cards">
                             <BlogParser feedUrl={blogUrl}/>
                         </div>
