908d8b39ca72fa507e731be044c05957b0652584
mritunjay kumar mani <mkumarmani97@gmail.com>
2025-02-11 20:29:03 +0530

    Update resume

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
 export const Constants = {
-    RESUME_S3_URL: "https://mritunjay-resume.s3.amazonaws.com/Mritunjay-SE-PG-Resume.pdf",
+    RESUME_S3_URL: "https://mritunjay-resume.s3.amazonaws.com/Mritunjay-BE-resume.pdf",
     BLOGS_FEED_URL: "https://medium.com/feed/@plusx0x07",
 }
